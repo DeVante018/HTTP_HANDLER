0a47685311b38a0d5981658c027fae59eedd4bb7
DeVante018 <devantef@buffalo.edu>
2021-03-26 15:45:48 -0400

    checks for all uploaded files to be posted on the webiste

--- src/on_get.py
+++ src/on_get.py
@@ -1,32 +1,72 @@
 import paths
+from containers import Statics
 
 
 def on_get(p_obj, dictionary) -> bytes:
     ret_data = ""
     if dictionary["path"] == p_obj.get_root:
-        file_path = "/Users/devantefrederick/IdeaProjects/HTTP_HANDLER/src/sample_page/index.html"
-        ret_data = build_response(p_obj, file_path, p_obj.content_text_html, False)
+        ret_data = check_images()
+        build_str = ""
+        build_str += p_obj.http_version + " " + p_obj.ok
+        build_str += p_obj.slash_rn
+        build_str += p_obj.content_length
+        build_str += str(len(bytes(ret_data, "utf8")))
+        build_str += p_obj.slash_rn
+        build_str += p_obj.content_text_html
+        build_str += p_obj.slash_rn
+        build_str += p_obj.no_sniff
+        build_str += p_obj.slash_rn + p_obj.slash_rn
+        convert_bytes = bytes(build_str, encoding='utf8')
+        convert_bytes += bytes(ret_data, encoding="utf8")
+        ret_data = convert_bytes
 
     elif dictionary["path"] == p_obj.styles_css:
-        file_path = "/Users/devantefrederick/IdeaProjects/HTTP_HANDLER/src/sample_page/style.css"
+        file_path = "src/sample_page/style.css"
         ret_data = build_response(p_obj, file_path, p_obj.content_text_css, False)
 
     elif dictionary["path"] == p_obj.functions_js:
-        file_path = "/Users/devantefrederick/IdeaProjects/HTTP_HANDLER/src/sample_page/functions.js"
+        file_path = "src/sample_page/functions.js"
         ret_data = build_response(p_obj, file_path, p_obj.content_text_javascript, False)
 
     elif dictionary["path"] == p_obj.utf_8:
-        file_path = "/Users/devantefrederick/IdeaProjects/HTTP_HANDLER/src/sample_page/utf.txt"
+        file_path = "src/sample_page/utf.txt"
         ret_data = build_response(p_obj, file_path, p_obj.content_text_plain, False)
 
     elif dictionary["path"].split("/")[1] == "image":
-        file_path = "/Users/devantefrederick/IdeaProjects/HTTP_HANDLER/src/sample_page"
+        file_path = "src/sample_page"
         file_path += dictionary["path"]
         ret_data = build_response(p_obj, file_path, p_obj.content_image_jpg, False)
     else:
         content = process_query(p_obj, dictionary["path"])
         ret_data = build_response(p_obj, content, p_obj.content_text_html, True)
     return ret_data
+
+
+def check_images():
+    home_page = "src/sample_page/index.html"
+    template_image = "<img src=\"/image/{REPLACE}\" style=\"width:500px;height:600px;\">"
+    template_caption = "<p>{REPLACE}</p><br>"
+    # print(template_image)
+    # landing = h.read
+    image_comment_block = ""
+    for images in Statics.captions_images:
+        image_comment_block += template_image.replace("{REPLACE}", images[1])
+        image_comment_block += "\n"
+        image_comment_block += template_caption.replace("{REPLACE}", images[0])
+        image_comment_block += "\n"
+
+    h = open(home_page, "r")
+    new_home_page = ""
+    inc = 0
+    for lines in h:
+        new_home_page += lines
+        if lines == "</form>\n":
+            inc += 1
+        if inc == 2:
+            new_home_page += image_comment_block
+            inc += 1
+    h.close()
+    return new_home_page
 
 
 def build_response(p_obj, file_path, content_type, _query) -> bytes:
@@ -70,14 +110,14 @@ def process_query(path_obj, path):
         images_query = images_query[1].split("+")
         for img in images_query:
             files_list.append(file + img + ".jpg")
-        read_file = open("/Users/devantefrederick/IdeaProjects/HTTP_HANDLER/src/sample_page/image_template.html",
+        read_file = open("src/sample_page/image_template.html",
                          "r")
         read_file = read_file.read()
         for x in range(len(files_list)):
-            #print(files_list[x])
+            # print(files_list[x])
             read_file = read_file.replace("{{img}}", files_list[x], 1)
         read_file = read_file.replace("{{discription}}", "Hello " + name_query[1] + " here are the requested images")
-        #print(read_file)
+        # print(read_file)
         return read_file
     else:
         return "Will implement 404 later"
