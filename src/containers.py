b8a5dd64c6e79d44de650c26197db99ab76d5d9b
DeVante018 <devantef@buffalo.edu>
2021-04-09 15:38:00 -0400

    created array containers for a list of connected clients, clients connected by websockets, and the chat history on the server.

--- src/containers.py
+++ src/containers.py
@@ -1,3 +1,6 @@
-# I will use this class as a shared data structure between
+# I will use this class to treat variable as if they are static
 class Statics:
     captions_images = []
+    server_clients = []
+    server_web_sockets = []
+    server_chat_history = []  # just store each sent web socket frame in this array
