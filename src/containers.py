81d1a3be83557def1cbd681a0ce3419812bec711
DeVante018 <devantef@buffalo.edu>
2021-04-19 17:34:07 -0400

    Added Mongodb database and put the chat collection as part of the Statics class

--- src/containers.py
+++ src/containers.py
@@ -1,6 +1,14 @@
 # I will use this class to treat variable as if they are static
+# also will setup my database here so it is accessible by all levels of the server
+import pymongo
+
+connection = pymongo.MongoClient("mongodb://my_database:27017")
+data_base = connection["CSE312"]
+chat_collection = data_base["chat_log"]
+
+
 class Statics:
+    chat = chat_collection
     captions_images = []
     server_clients = []
     server_web_sockets = []
-    server_chat_history = []  # just store each sent web socket frame in this array
