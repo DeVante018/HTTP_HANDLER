68367b2701732df9d902dc3bfad759b9fe458c40
DeVante018 <devantef@buffalo.edu>
2021-04-23 18:45:23 -0400

    Cookie parsing and validation with verification

--- src/http_handler.py
+++ src/http_handler.py
@@ -1,13 +1,7 @@
+from containers import Statics
 from paths import Paths
 from on_get import Get
 from on_post import Post
-
-# when running on my machine
-# /Users/devantefrederick/IdeaProjects/HTTP_HANDLER/src/sample_page/index.html
-# /Users/devantefrederick/IdeaProjects/HTTP_HANDLER/src/sample_page/functions.js
-# /Users/devantefrederick/IdeaProjects/HTTP_HANDLER/src/sample_page/style.css
-# /Users/devantefrederick/IdeaProjects/HTTP_HANDLER/src/sample_page/utf.txt
-# /Users/devantefrederick/IdeaProjects/HTTP_HANDLER/src/sample_page/image/flamingo.jpg
 
 
 class RequestHandler:
@@ -35,7 +29,7 @@ class RequestHandler:
         # will get more headers later if I need them
         headers_map["request_type"] = request_type  # should be GET or POST
         headers_map["path"] = path  # path searched for
-        headers_map["version"] = version # HTTP version the browser is using
+        headers_map["version"] = version  # HTTP version the browser is using
         i = 0
         for headers in split_data:
             if i != 0:
@@ -45,9 +39,35 @@ class RequestHandler:
 
         # print(headers_map)
         return_bytes = bytes
+        has_cookie = False
+        parse_cookie = []
+        Statics.token = ""
+        for keys in headers_map:
+            if str(keys) == 'Cookie':
+                has_cookie = True
+        if has_cookie:
+            parse_cookie = headers_map['Cookie']
+            parse_cookie = parse_cookie.split(";")
+            length = len(parse_cookie)
+        else:
+            length = 0
 
+        if length == 2:
+            parse_cookie = parse_cookie[1].split("=")
+            Statics.current_cookie = parse_cookie[1]
+        elif length == 3:
+            auth_cookie = parse_cookie[2].split("=")
+            Statics.token = auth_cookie[1]
+        else:
+            if len(parse_cookie) != 0 and parse_cookie[0][0] == 'i':
+                # this is firefox doing w.e tf it wants
+                parse_cookie = parse_cookie[0].split("=")
+                if len(parse_cookie) > 1:
+                    Statics.current_cookie = parse_cookie[1]
+
+            Statics.current_cookie = ""
         if headers_map["request_type"] == "GET":
-            return_bytes = self.get_handler.process(headers_map,socket)
+            return_bytes = self.get_handler.process(headers_map, socket)
         elif headers_map["request_type"] == "POST":
             return_bytes = self.post_handler.process(socket, headers_map, split_array, split_len)
         # process the request based on what it is
