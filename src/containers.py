7f6112f2bd7cd4760527f66a00e57f5283d57bfd
DeVante018 <devantef@buffalo.edu>
2021-04-23 18:44:08 -0400

    created collections for cookies, users, and authorized user tokens

--- src/containers.py
+++ src/containers.py
@@ -2,13 +2,21 @@
 # also will setup my database here so it is accessible by all levels of the server
 import pymongo
 
-connection = pymongo.MongoClient("mongodb://my_database:27017")
+connection = pymongo.MongoClient("mongodb://localhost:27017")
 data_base = connection["CSE312"]
 chat_collection = data_base["chat_log"]
+cookies_collection = data_base["cookies"]
+usernames_passwords = data_base["users"]
+authorization_tokens = data_base["authorized"]
 
 
 class Statics:
+    token = ""
+    current_cookie = ""
     chat = chat_collection
+    db_cookies = cookies_collection  # Oreo's, ChipsAhoy, Pringles, ect
+    usr_pwd = usernames_passwords
+    auth = authorization_tokens
     captions_images = []
     server_clients = []
     server_web_sockets = []
