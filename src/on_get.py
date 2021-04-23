32dfd5c70e837021d0f3e7dfaf5b69f1beeed931
DeVante018 <devantef@buffalo.edu>
2021-04-23 18:46:46 -0400

    Checking the "state" of the client who's accessing the server

--- src/on_get.py
+++ src/on_get.py
@@ -1,23 +1,43 @@
+import string
+from strgen import StringGenerator as SG
 import paths
 import web_sock
+import Authentication
 from containers import Statics
 
 
 def on_get(p_obj, dictionary, socket) -> bytes:
     ret_data = ""
+    visit_cookie = False
+    cookie = generate_cookie()
+    client_id = socket.client_address[0].replace(".", "_")
+    logged_in = Authentication.check_authentication()
+
     if dictionary["path"] == p_obj.get_root:
-        ret_data = check_images()
+        if logged_in['status']:
+            return signed_in(p_obj, logged_in['name'])
+        if Statics.current_cookie != "":
+            visit_cookie = Authentication.check_cookie(client_id)
+        print("COOOKIE: ", visit_cookie)
+        ret_data = check_images(visit_cookie)
         build_str = ""
         build_str += p_obj.http_version + " " + p_obj.ok
+        build_str += p_obj.slash_rn
+        build_str += p_obj.content_text_html
         build_str += p_obj.slash_rn
         build_str += p_obj.content_length
         build_str += str(len(bytes(ret_data, "utf8")))
         build_str += p_obj.slash_rn
-        build_str += p_obj.content_text_html
-        build_str += p_obj.slash_rn
-        build_str += p_obj.set_cookie
-        build_str += generate_cookie()
         build_str += p_obj.no_sniff
+        build_str += p_obj.slash_rn
+        if not visit_cookie:
+            build_str += p_obj.set_cookie
+            print("\n---cookie insert\n")
+            print()
+            print(cookie)
+            print("\n---cookie insert\n")
+            build_str += cookie
+            Statics.db_cookies.insert_one({"client_id": client_id, "cookie": cookie})
         build_str += p_obj.slash_rn + p_obj.slash_rn
         convert_bytes = bytes(build_str, encoding='utf8')
         convert_bytes += bytes(ret_data, encoding="utf8")
@@ -40,16 +60,20 @@ def on_get(p_obj, dictionary, socket) -> bytes:
         file_path += dictionary["path"]
         ret_data = build_response(p_obj, file_path, p_obj.content_image_jpg, False)
 
+    elif dictionary["path"] == p_obj.signup:
+        file_path = "../src/sample_page/signup.html"
+        ret_data = build_response(p_obj, file_path, p_obj.content_text_html, False)
+
     elif dictionary["path"] == p_obj.socket:
         ret_data = web_sock.upgrade_connection(dictionary['Sec-WebSocket-Key'])
         socket.request.sendall(ret_data)
-        client_id = socket.client_address[0] + str(socket.client_address[1])
+        print("client address: ", client_id + "\n")
         Statics.server_clients.append(client_id)
         Statics.server_web_sockets.append(socket.request)
         # send all the chat history to newly connected client
         all_messages = Statics.chat.find()
         for queries in all_messages:
-            print(queries['message'])
+            # print(queries['message'])
             socket.request.sendall(queries['message'])
         # now we have to keep the socket connection open
         web_sock.read_socket(socket)
@@ -59,7 +83,7 @@ def on_get(p_obj, dictionary, socket) -> bytes:
     return ret_data
 
 
-def check_images():
+def check_images(is_cookie):
     home_page = "../src/sample_page/index.html"
     template_image = "<img src=\"/image/{REPLACE}\" style=\"width:300px;height:300px;\">"
     template_caption = "<p>{REPLACE}</p><br>"
@@ -76,7 +100,14 @@ def check_images():
     new_home_page = ""
     inc = 0
     for lines in h:
-        new_home_page += lines
+        if is_cookie:
+            #print(lines)
+            if lines == "<h1>Welcome to this website!!</h1>\n":
+                new_home_page += "<h1>Welcome back because you have a session cookie set!!!!!!</h1>\n"
+            else:
+                new_home_page += lines
+        else:
+            new_home_page += lines
         if lines == "</form>\n":
             inc += 1
         if inc == 2:
@@ -138,6 +169,37 @@ def process_query(path_obj, path):
         return read_file
     else:
         return "Will implement 404 later"
+
+
+def signed_in(p, name):
+    build_str = ""
+    build_str += p.http_version + " " + p.ok
+    build_str += p.slash_rn
+    build_str += p.content_length
+    f = open("../src/sample_page/signed_in.html", 'r')
+    read_file = ""
+    for lines in f:
+        if lines == '<h1>$$replaceMe$$</h1>\n':
+            print("LINE: ", lines)
+            read_file += '<h1>you are logged in as ' + name + ' </h1>'
+        else:
+            read_file += lines
+
+    length = len(read_file)
+    f.close()
+    build_str += str(length)
+    build_str += p.slash_rn
+    build_str += p.content_text_html
+    build_str += p.slash_rn
+    build_str += p.no_sniff
+    build_str += p.slash_rn + p.slash_rn
+    convert_bytes = bytes(build_str, encoding='utf8')
+    convert_bytes += read_file.encode()
+    return convert_bytes
+
+
+def generate_cookie():
+    return SG(r"[\w]{30}").render()  # returns random string
 
 
 class Get:
